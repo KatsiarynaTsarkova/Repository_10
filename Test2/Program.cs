3a25ee93f556f93cb7a4ae1be03be84fb18ab290
Kate <katya_tsarkov@mail.ru>
2022-10-02 22:18:45 +0300

    Test2

---
+++ Test2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+  
+ int SumNumbers(int M, int N)
+        {
+            if (M == N) return M;                       
+            if (M < N) return N + SumNumbers(M, N - 1); 
+            else return N + SumNumbers(M, N + 1);        
+        }
+ Console.Write("M = ");
+ int M = int.Parse(Console.ReadLine());
+ Console.Write("N = ");
+ int N = int.Parse(Console.ReadLine());
+ Console.WriteLine($"{SumNumbers(M, N)}");
+ Console.ReadLine();
+        
